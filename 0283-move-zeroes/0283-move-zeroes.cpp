93de2769fcbcd720019ca3e08ee38bfa582a1586
Rishabh Upadhyay <rishabh.p2326@gmail.com>
2026-07-28 23:19:40 +0530

    Time: 3 ms (14.65%), Space: 23.7 MB (83.4%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        if(nums.size()==1 && nums[0]==0) return ;
+        //if(nums.size()==1 && nums[0]==0) return ;
         int zeros = count(nums.begin(),nums.end(),0);
         for(auto it = nums.begin() ; it!=nums.end() ; )
             if(*it==0) nums.erase(it) ;
