e16c3f374e5757b6a5bee5a216478026512c0e4b
Rishabh Upadhyay <rishabh.p2326@gmail.com>
2026-07-30 22:11:45 +0530

    Time: 7 ms (18.64%), Space: 9.8 MB (50.2%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end()) ;
-        sort(t.begin(),t.end()) ;
-        return s==t ;
+        if(s.size()!=t.size()) return false ;
+        map<char,int> freq ;
+        for(int i = 0 ; i < s.size() ; i++)
+            freq[s[i]]++;
+        for(int i = 0 ; i < t.size() ; i++)
+            if(freq.count(t[i]) && freq[t[i]]>0)
+                freq[t[i]]--;
+            else return false;
+        return true ; 
     }
 };
