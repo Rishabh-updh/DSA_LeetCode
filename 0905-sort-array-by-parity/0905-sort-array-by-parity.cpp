36ad2b9ec3889f9748df31a39eba80012688c0d6
Rishabh Upadhyay <rishabh.p2326@gmail.com>
2026-08-17 00:03:21 +0530

    Time: 53 ms (4.06%), Space: 21.8 MB (5.86%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -1,10 +1,50 @@
 class Solution {
+    typedef struct node{
+        int val ;
+        node* next ;
+    } node;
+    node* push_front(node* head,int val){
+        if(head==nullptr){
+            node* newNode = new node ;
+            newNode->val = val ;
+            newNode->next = nullptr ;
+            head = newNode ;
+            return head ;
+        }
+        node* newNode = new node ;
+        newNode->val = val ;
+        newNode->next = head ;
+        head = newNode ;
+        return head ;
+    }
+    node* push_back(node* head,int val){
+         if(head==nullptr){
+            node* newNode = new node ;
+            newNode->val = val ;
+            newNode->next = nullptr ;
+            head = newNode ;
+            return head ;
+        }
+        node* newNode = new node ;
+        newNode->val = val ;
+        newNode->next = nullptr ;
+        node* temp = head ;
+        while(temp->next != nullptr)
+            temp = temp->next;
+        temp->next = newNode ;
+        return head ;
+    }
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         vector<int> v = {} ;
+        node* head = nullptr ;
         for(auto in : nums){
-            if(in%2==0) v.insert(v.begin(),in) ;
-            else v.push_back(in);
+            if(in%2==0) head = push_front(head,in);
+            else head = push_back(head,in);
+        }
+        while(head!=nullptr){
+            v.push_back(head->val);
+            head=head->next;
         }
         return v ;
     }
